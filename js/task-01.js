0482eebd1b1bda7e01ee3ee179925de9d7664957
unknown <bogdan.titsky@gmail.com>
2023-04-26 12:39:37 +0300

    fix2

--- js/task-01.js
+++ js/task-01.js
@@ -4,8 +4,8 @@ const itemEl = listEl.children;
 console.log(`Number of categories: ${itemEl.length}`);
 
 Array.from(itemEl).forEach((item) => {
-    const category = item.querySelector('h2').textContent;
-    const liCount = item.querySelectorAll('li').length;
+    const category = item.firstElementChild.textContent;
+    const liCount = item.lastElementChild.children.length;
 
     console.log(`Category: ${category}`);
     console.log(`Elements: ${liCount}`);
